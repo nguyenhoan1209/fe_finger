8fa790e1a98f7dae3a4bbc37572b131eaa13a466
Quang Dong <quangdongne88@gmail.com>
2024-04-22 10:12:43 +0700

    refactor:made static folder homepage,loginpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import LoginPage from './pages/LoginPage'
-import HomePage from './pages/HomePage'
+import LoginPage from "./pages/LoginPage/LoginPage";
+import HomePage from "./pages/HomePage/HomePage";
 
-import { AuthProvider } from './context/AuthContext'
- 
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <div className="App">
-        <Router>
-          <AuthProvider>
-            <Routes>
-                <Route path="/" element={<HomePage/>} />
-                <Route path="/login" element={<LoginPage/>}/>
-            </Routes>
-            </AuthProvider>
-          </Router>
-    </div>  
-  )
-}
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
