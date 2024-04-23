57c889c0722aa7ee1b607507973520e1ba99b161
nguyenhoan1209 <nguyenvanhoan0902@gmail.com>
2024-04-23 11:13:30 +0700

    fix: log out button navigate

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,16 @@
 import React, { useContext } from "react";
 import AuthContext from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-  const [token] = useContext(AuthContext);
+  const [token, setToken] = useContext(AuthContext);
+  const navigate = useNavigate()
 
   const logoutUser = () => {
     // e.preventDefault()
     localStorage.removeItem("authToken");
     setToken(null);
-    navigate("/login");
+    navigate("/");
   };
 
   return (
@@ -61,11 +63,9 @@ const Sidebar = () => {
                         alt="user photo"
                       />
                       <button
-                        type="button"
                         onClick={logoutUser}
                         className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                       >
-                        {" "}
                         Log out
                       </button>
                     </div>
